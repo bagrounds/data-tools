c84f10b58966cd14d05aef0e4f7a72c3aae72f22
bgrounds <bgrounds@ea.com>
2016-06-02 15:59:20 -0700

    [ADD] delimiter option to format.jsonToCsv()

--- test/test-format.js
+++ test/test-format.js
@@ -45,6 +45,36 @@
           done()
         }
       })
+      it('it should convert json to tsv', function (done) {
+        var jsonInput = [
+          {column1: 'a string', column2: 9, column3: null},
+          {column1: 'another string', column2: 42, column3: null},
+          {column1: 'abc', column2: null, column3: 198},
+          {column1: null, column2: 98, column3: 5}
+        ]
+
+        var expectedCsvOutput = 'column1\tcolumn2\tcolumn3\n'
+        expectedCsvOutput += 'a string\t9\t\n'
+        expectedCsvOutput += 'another string\t42\t\n'
+        expectedCsvOutput += 'abc\t\t198\n'
+        expectedCsvOutput += '\t98\t5\n'
+
+        var options = {
+          dataSet: jsonInput,
+          delimiter: '\t'
+        }
+
+        format.jsonToCsv(options, handleResults)
+
+        function handleResults (error, csv) {
+          error && console.error(error)
+          expect(error).not.to.be.ok
+
+          expect(csv).to.equal(expectedCsvOutput)
+
+          done()
+        }
+      })
     })
   })
 })()
